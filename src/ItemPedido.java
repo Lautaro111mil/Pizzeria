623824662b42ff630f11443500ba3fc11a735d79
Santiago <santidevargas@gmail.com>
2018-05-14 09:28:34 -0300

    calcula costo con mas de 1 pizza

--- src/ItemPedido.java
+++ src/ItemPedido.java
@@ -11,16 +11,33 @@
 public class ItemPedido {
 
     private Pizza pizza;
-    private int cantidad;
+    private long cantidad;
 
-    public ItemPedido(Pizza pizza, int cantidad) {
-        if (cantidad <= 0) {
-            throw new Error("La cantidad ingresada debe ser mayor a 0");
-        }
+    public ItemPedido(Pizza pizza, long cantidad) {
+          if (cantidad <= 0) {
+          throw new Error("La cantidad ingresada debe ser mayor a 0");
+       }
         this.pizza = pizza;
         this.cantidad = cantidad;
     }
 
+    public Pizza getPizza() {
+        return pizza;
+    }
+
+    public void setPizza(Pizza pizza) {
+        this.pizza = pizza;
+    }
+
+    public long getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(long cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    
     public double calcularCosto() {
         return pizza.calcularCosto() * cantidad;
     }
