d07d88e2bd23dd1a2905498e25e43ee71dd7c597
Lautaro111mil <lautarosoto2015@gmail.comi7>
2018-05-28 08:45:35 -0300

    Se implementaron metodos del modelo

--- src/ModeloListaPedidos.java
+++ src/ModeloListaPedidos.java
@@ -9,15 +9,15 @@ import javax.swing.table.TableModel;
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  *
  * @author User
  */
-public class ModeloListaPedidos implements TableModel{
-    
-    private List<Pedido> pedidos=new ArrayList<Pedido>();
-    
+public class ModeloListaPedidos implements TableModel {
+
+    private List<Pedido> pedidos = new ArrayList<Pedido>();
+    private List<TableModelListener> listener = new ArrayList<TableModelListener>();
+
     @Override
     public int getRowCount() {
         return pedidos.size(); //To change body of generated methods, choose Tools | Templates.
@@ -30,20 +30,19 @@ public class ModeloListaPedidos implements TableModel{
 
     @Override
     public String getColumnName(int columnIndex) {
-        String nombreColumna="";
-        if (columnIndex==0){
-            nombreColumna= "Cliente";            
-        }else if(columnIndex==1){
-            nombreColumna="Hora";
+        String nombreColumna = "";
+        if (columnIndex == 0) {
+            nombreColumna = "Cliente";
+        } else if (columnIndex == 1) {
+            nombreColumna = "Hora";
+        } else if (columnIndex == 2) {
+            nombreColumna = "Minutos";
+        } else if (columnIndex == 3) {
+            nombreColumna = "Demora";
+        } else if (columnIndex == 4) {
+            nombreColumna = "Costo";
         }
-        else if (columnIndex==2){
-            nombreColumna="Minutos";
-        }else if (columnIndex==3){
-            nombreColumna="Demora";
-        }else if (columnIndex==4){
-            nombreColumna="Costo";
-        }
-        
+
         return nombreColumna;
     }
 
@@ -60,37 +59,37 @@ public class ModeloListaPedidos implements TableModel{
     @Override
     public Object getValueAt(int rowIndex, int columnIndex) {
         Pedido pedido = pedidos.get(rowIndex);
-        String valor="";
-        if (columnIndex==0){
-            valor=pedido.getNombreCliente();
-        }else if (columnIndex==1){
-            String hora=(String) String.valueOf(pedido.getHoraPedido());
-            valor=hora;
-        }else if (columnIndex==2){
-            String minutos=(String)String.valueOf(pedido.getMinutosPedido());
-            valor=minutos;
-        }else if (columnIndex==3){
-            String demora=(String) String.valueOf(pedido.getMinutosDemora());
-            valor=demora;
-        }else if (columnIndex==4){
-            String costo=(String)String.valueOf(pedido.getItemPedido().calcularCosto());
+        String valor = "";
+        if (columnIndex == 0) {
+            valor = pedido.getNombreCliente();
+        } else if (columnIndex == 1) {
+            String hora = (String) String.valueOf(pedido.getHoraPedido());
+            valor = hora;
+        } else if (columnIndex == 2) {
+            String minutos = (String) String.valueOf(pedido.getMinutosPedido());
+            valor = minutos;
+        } else if (columnIndex == 3) {
+            String demora = (String) String.valueOf(pedido.getMinutosDemora());
+            valor = demora;
+        } else if (columnIndex == 4) {
+            String costo = (String) String.valueOf(pedido.getItemPedido().calcularCosto());
         }
         return pedido;
     }
 
     @Override
     public void setValueAt(Object aValue, int rowIndex, int columnIndex) {
-         //To change body of generated methods, choose Tools | Templates.
+        //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
     public void addTableModelListener(TableModelListener l) {
-        
+    this.listener.add(l);
     }
 
     @Override
     public void removeTableModelListener(TableModelListener l) {
+        this.listener.remove(l);
     }
-    
-    
+
 }
